047851991dbe65eb9dad4d1c370b224eac4ef508
marcin7848 <marcin7848@gmail.com>
2019-04-28 18:20:44 +0200

    start configuration

---
+++ src/main/java/com/bookTicket/BookTicketApplication.java
@@ -0,0 +1,13 @@
+package com.bookTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookTicketApplication.class, args);
+    }
+
+}
