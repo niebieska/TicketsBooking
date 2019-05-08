bc6b00e95f4f6a0f0fb1bf6ac9843124ac56aacf
marcin7848 <marcin7848@gmail.com>
2019-05-08 22:48:46 +0200

    repertory added

--- src/client/src/app/movie-management/movie-management.module.ts
+++ src/client/src/app/movie-management/movie-management.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieComponent } from './movie/movie.component';
 import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from "@angular/material";
+import {MatButtonModule, MatIconModule} from "@angular/material";
 
 @NgModule({
   declarations: [MovieComponent],
   imports: [
     CommonModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class MovieManagementModule { }
